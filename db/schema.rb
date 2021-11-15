aac4a604bc99b41af92f6f95b097d4f3fc1a739f
Sei Fukushima <62275640+replica531@users.noreply.github.com>
2021-11-16 01:42:48 +0900

    パスワード追加 (#7)
    * homeページ追加
    
    * static page追加
    
    * test色付け
    
    * full_titleヘルパーメソッド
    
    * レイアウト整理#
    
    * Userモデルvalidation
    
    * User パスワード
    
    * User password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_074128) do
+ActiveRecord::Schema.define(version: 2021_11_15_162636) do
 
   create_table "musics", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2021_11_13_074128) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
