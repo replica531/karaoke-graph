c9c0c5ffb8c0b3acb053e970911bea0eac956c44
Sei Fukushima <62275640+replica531@users.noreply.github.com>
2021-11-16 02:59:45 +0900

    User signup (#8)
    * homeページ追加
    
    * static page追加
    
    * test色付け
    
    * full_titleヘルパーメソッド
    
    * レイアウト整理#
    
    * Userモデルvalidation
    
    * User パスワード
    
    * User password
    
    * 登録時エラー
    
    * use SSL

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar"))
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -51,16 +51,8 @@ class UserTest < ActiveSupport::TestCase
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
-  end
-
-  test "email addresses should be saved as lower-case" do
-    mixed_case_email = "Foo@ExAMPle.CoM"
-    @user.email = mixed_case_email
-    @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should be present (nonblank)" do
