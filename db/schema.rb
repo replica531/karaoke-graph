347c2b45a0ecd1de852ab179b542a9b346e15e07
Sei Fukushima <sftakkyu1283@gmail.com>
2022-08-11 23:33:01 +0900

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_528_100_620) do
+ActiveRecord::Schema.define(version: 20_220_811_142_858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -54,6 +52,7 @@ ActiveRecord::Schema.define(version: 20_220_528_100_620) do
     t.datetime 'activated_at'
     t.string 'reset_digest'
     t.datetime 'reset_sent_at'
+    t.string 'default_model', default: 'その他', null: false
     t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
